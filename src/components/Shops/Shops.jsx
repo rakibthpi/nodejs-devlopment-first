199a29c89c25c3f978550f00e5aa8e133e9b44a7
rakib <rakibhosen2244@gmail.com>
2023-08-29 02:31:46 +0600

    shop page create and see the design

---
+++ src/components/Shops/Shops.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Shops = () => {
+    return (
+        <div className="shoping_main">
+            <h2>This is shop js </h2>
+        </div>
+    );
+};
+
+export default Shops;
